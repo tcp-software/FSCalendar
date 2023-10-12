574dc953ef4bd00a6b5617255ac66413fe4f9e42
Milan Bibeskovic <milan.bibeskovic@humanity.com>
2023-10-12 13:28:28 +0200

    - spm support part 2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FSCalendar"),
+            name: "FSCalendar",
+            dependencies: [],
+            path: "FSCalendar/",
+            exclude: ["Info.plist"]),
         .testTarget(
             name: "FSCalendarTests",
             dependencies: ["FSCalendar"]),
